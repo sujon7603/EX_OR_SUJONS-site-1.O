df37d5b137074d97f2735de4d271c14f2c25df7b
Hasanul Islam Sujon <68911980+sujon7603@users.noreply.github.com>
2021-01-03 01:07:29 +0600

    Add files via upload

---
+++ EX_OR_SUJONS site 1.O/js/main.js
@@ -0,0 +1,11 @@
+//BX SLIDER //
+
+
+
+  $(document).ready(function(){
+    $('.slider-area').bxSlider();
+      $('#main-menu').meanmenu({
+          meanMenuContainer: '#header-nav',
+          meanScreenWidth:991,
+      });
+  });
